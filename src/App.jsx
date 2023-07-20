0d0843437c3378b37d564f7a9e485135517b8309
Baibhav KC <baibhavkc11@gmail.com>
2023-07-20 23:52:55 +0545

    add react-query and routing to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import { useState } from 'react';
+import React from 'react';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import { ProductProvider } from './context/ProductContext';
+import ProductDetail from './pages/ProductDetail';
+import Home from './pages/Home';
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <BrowserRouter>
+      <ProductProvider>
+        <Navbar />
+        <div id='App'>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/product/:id' element={<ProductDetail />} />
+          </Routes>
+        </div>
+      </ProductProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
